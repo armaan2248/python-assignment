a4f3cbb85eb73396a9e5c0df304fc627d76353ee
armaan2248 <armaanhshs@gmail.com>
2025-05-15 11:50:00 +0530

    Add files via upload

---
+++ Assignment 1 TASK 2.py
@@ -0,0 +1,10 @@
+#TASK 2
+# Taking user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating full name
+full_name = first_name + " " + last_name
+
+# Displaying personalized greeting
+print("Hello,", full_name + "! Welcome!")
